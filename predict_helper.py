097b611fa38c3827c22cccad352d5364a550f79e
Siddartha Reddy <siddarthareddy.turpu@gmail.com>
2018-04-10 14:11:27 -0700

    1. Added Ner feature input to the model. 2. Refactored the code a bit.

---
+++ predict_helper.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  9 23:41:35 2018
+
+@author: siddartha
+"""
+from nltk.corpus import wordnet as wn
+import numpy as np
+import data_helpers
+import auxilary_data_helper
+from tokenizeData import Lemmatizer
+import itertools
+import pickle
+from keras.models import load_model
+
+def evaluate_on_test(x_test, y_test, model):
+    predictions = model.predict(x_test)
+    evaluate = model.evaluate(x_test, y_test)
+    #evaluate = model.evaluate(testStr, np.array([[0,1], [0,1],[0,1], [1, 0]]))
+    print("Evaluated against test dataset: " + evaluate)
+
+
+def predict_movie(testStr, model, labels_dict, vocabulary, event_voc, ners_voc, multiple = False):
+    testStr_vector = transform_testdata([testStr], vocabulary)
+    events_onehot = extract_events_onehot([testStr], event_voc)
+    ners_onehot = extract_ners_onehot([testStr], ners_voc)
+
+    pred = model.predict([testStr_vector, events_onehot, ners_onehot])
+    
+    sorted_pred = pred[0]
+    
+    if(not multiple):
+        print(labels_dict[sorted_pred.argmax()])
+        return
+    pred_dict = {i: x for i, x in enumerate(sorted_pred)}
+    sorted_pred_dict = sorted(pred_dict, key=pred_dict.get, reverse=True)
+    sliced_pred = sorted_pred_dict[:5]
+    for r in sliced_pred:
+        print(labels_dict[r] + str(pred_dict[r]))
+
+def transform_testdata(test_strs, vocabulary):
+    test_strs = [Lemmatizer(data_helpers.clean_str(sent)) for sent in test_strs]
+    test_strs = [s.split(" ") for s in test_strs]
+    
+    test_strs_padded = data_helpers.pad_sentences(test_strs, testStringLength = 90)
+    x = np.array([[vocabulary[word] if word in vocabulary else vocabulary["<PAD/>"] for word in sentence] for sentence in test_strs_padded])
+    return x
+
+ 
+def extract_events_onehot(testStrs, event_voc):
+    events_all_test_str = []
+    for testStr in testStrs:
+        sentences = testStr.split('.')
+        events_in_str = filter(None, auxilary_data_helper.extract_events(sentences))
+        events_all_test_str.append(list(set(events_in_str)))
+        
+    events_all_test_str = auxilary_data_helper.pad_sentences(events_all_test_str, testStringLength=1379)
+    synonyms = get_synonyms(events_all_test_str)
+    return auxilary_data_helper.build_input_data(events_all_test_str, event_voc, synonyms=synonyms, is_test = True)
+    
+def extract_ners_onehot(testStrs, ners_voc):
+     ners_all_test_str = []
+     for testStr in testStrs:
+         ners_in_str = auxilary_data_helper.extract_ners(testStr)
+         ners_all_test_str.append(ners_in_str)
+     ners_all_test_str = auxilary_data_helper.pad_sentences(ners_all_test_str, testStringLength=1379)
+     return auxilary_data_helper.build_input_data(ners_all_test_str, ners_voc)
+
+def get_synonyms(events_all_summaries):
+    synonyms ={event: get_synonyms_for_word(event) for event in list(itertools.chain.from_iterable(events_all_summaries))}
+    return synonyms        
+    
+def get_synonyms_for_word(word):
+    synonyms = []
+    for syn in wn.synsets(word):
+        for l in syn.lemmas():
+            synonyms.append(l.name())
+    return synonyms
+
+def load_model_and_params():
+    with open ('model_params', 'rb') as fp:
+        model_params = pickle.load(fp)
+    model = load_model('model.h5')
+    return model, model_params
+    
--- sentiment_cnn.py
+++ sentiment_cnn.py
@@ -22,17 +22,14 @@ Differences from original article:
 import numpy as np
 import data_helpers
 from w2v import train_word2vec
-import EventDataHelper
-from keras.models import Sequential, Model
+import auxilary_data_helper
+from keras.models import Model
 from keras.layers import Dense, Dropout, Flatten, Input, MaxPooling1D, Convolution1D, Embedding
 from keras.layers.merge import Concatenate
-from keras.datasets import imdb
-from keras.preprocessing import sequence
 from keras.layers.merge import concatenate
 from keras.utils import plot_model
-import itertools
-from tokenizeData import Lemmatizer
-
+import predict_helper
+import pickle
 
 np.random.seed(0)
 
@@ -55,7 +52,7 @@ max_words = 5000
 min_word_count = 1
 context = 10
 events_seq_length = 300
-
+ners_seq_length = 300
 
 # ---------------------- Parameters end -----------------------
 def transform_events_input(event_all_summaries, labels_y, labels_dict):
@@ -67,78 +64,53 @@ def load_data():
     vocabulary_inv = {key: value for key, value in enumerate(vocabulary_inv_list)}
     labels_y = y.argmax(axis=1)
     
-    events_onehot, event_voc, event_voc_inv_list, events_labels_dict = EventDataHelper.load_event_data()
+    events_onehot, event_voc, event_voc_inv_list, events_labels_dict = auxilary_data_helper.load_event_data()
     event_voc_inv = {key: value for key, value in enumerate(event_voc_inv_list)}
-    x2 = transform_events_input(events_onehot, labels_y, labels_dict)
+    event_x = transform_events_input(events_onehot, labels_y, labels_dict)
+    
+    ners_onehot, ners_voc, ners_voc_inv_list = auxilary_data_helper.load_ners_data()
+    ners_voc_inv = {key: value for key, value in enumerate(ners_voc_inv_list)}
+    ners_x = transform_events_input(ners_onehot, labels_y, labels_dict)
     
     # Shuffle data
     shuffle_indices = np.random.permutation(np.arange(len(y)))
+    
     x = x[shuffle_indices]
-    x2 = x2[shuffle_indices]
+    event_x = event_x[shuffle_indices]
+    ners_x = ners_x[shuffle_indices]
+    
     y = y[shuffle_indices]
     train_len = int(len(x) * 0.9)
+    
     x_train = x[:train_len]
-    x2_train = x2[:train_len]
+    event_x_train = event_x[:train_len]
+    ners_x_train = ners_x[:train_len]
     y_train = y[:train_len]
+    
     x_test = x[train_len:]
-    x2_test = x2[train_len:]
+    event_x_test = event_x[train_len:]
+    ners_x_test = ners_x[train_len:]
     y_test = y[train_len:]
 
-    return x_train,  x2_train, y_train, x_test, x2_test,  y_test, vocabulary, vocabulary_inv, event_voc, event_voc_inv, num_labels, labels_dict
+    return x_train, event_x_train, ners_x_train, y_train, x_test, event_x_test,  ners_x_test, y_test, vocabulary, vocabulary_inv, event_voc, event_voc_inv, ners_voc, ners_voc_inv, num_labels, labels_dict
 
-def evaluate_on_test(x_test, y_test):
-    predictions = model.predict(x_test)
-    evaluate = model.evaluate(x_test, y_test)
-    #evaluate = model.evaluate(testStr, np.array([[0,1], [0,1],[0,1], [1, 0]]))
-    print("Evaluated against test dataset: " + evaluate)
-    
-def extract_events_onehot(testStrs, event_voc):
-    events_all_test_str = []
-    for testStr in testStrs:
-        sentences = testStr.split('.')
-        events_in_str = filter(None, EventDataHelper.extract_events(sentences))
-        events_all_test_str.append(list(set(events_in_str)))
-        
-    events_all_test_str = EventDataHelper.pad_event_sentences(events_all_test_str, testStringLength=1379)
-    return EventDataHelper.build_input_data(events_all_test_str, event_voc)
-    
-def predict_movie(testStr, model, labels_dict, event_voc, multiple = False):
-    testStr_vector = transform_testdata([testStr])
-    events_onehot = extract_events_onehot([testStr], event_voc)
-    pred = model.predict([testStr_vector, events_onehot])
-    
-    sorted_pred = pred[0]
-    
-    if(not multiple):
-        print(labels_dict[sorted_pred.argmax()])
-        return
-    pred_dict = {i: x for i, x in enumerate(sorted_pred)}
-    sorted_pred_dict = sorted(pred_dict, key=pred_dict.get, reverse=True)
-    sliced_pred = sorted_pred_dict[:5]
-    for r in sliced_pred:
-        print(labels_dict[r])
-
-def transform_testdata(test_strs):
-    test_strs = [Lemmatizer(data_helpers.clean_str(sent)) for sent in test_strs]
-    test_strs = [s.split(" ") for s in test_strs]
-    
-    test_strs_padded = data_helpers.pad_sentences(test_strs, testStringLength = 90)
-    x = np.array([[vocabulary[word] for word in sentence] for sentence in test_strs_padded])
-    return x
-
-    
+  
     
 # Data Preparation
 print("Load data...")
-x_train,  x2_train, y_train, x_test, x2_test,  y_test, vocabulary, vocabulary_inv, event_voc, event_voc_inv, num_labels, labels_dict = load_data()
+x_train, event_x_train, ners_x_train, y_train, x_test, event_x_test,  ners_x_test, y_test, vocabulary, vocabulary_inv, event_voc, event_voc_inv, ners_voc, ners_voc_inv,  num_labels, labels_dict = load_data()
 
 if sequence_length != x_test.shape[1]:
     print("Adjusting sequence length for actual size: {:d}".format(x_test.shape[1]))
     sequence_length = x_test.shape[1]
 
-if events_seq_length != x2_test.shape[1]:
-    print("Adjusting event sequence length for actual size: {:d}".format(x2_test.shape[1]))
-    events_seq_length = x2_test.shape[1]
+if events_seq_length != event_x_test.shape[1]:
+    print("Adjusting event sequence length for actual size: {:d}".format(event_x_test.shape[1]))
+    events_seq_length = event_x_test.shape[1]
+
+if ners_seq_length != ners_x_test.shape[1]:
+    print("Adjusting ners sequence length for actual size: {:d}".format(ners_x_test.shape[1]))
+    ners_seq_length = ners_x_test.shape[1]
     
 print("x_train shape:", x_train.shape)
 print("x_test shape:", x_test.shape)
@@ -174,12 +146,15 @@ events_input_layer = Input(shape=(events_seq_length,), name="event_input_layer")
 events_dense = Dense(int((events_seq_length/2)), activation="relu", name="event_dense_layer")(events_input_layer)
 #flatted_events = Flatten(name="event_flatten_layer")(events_dense)
 
-merged = concatenate([flated_conv_layers, events_dense], name="conv_event_merge_layer")
+ners_input_layer = Input(shape=(ners_seq_length,), name="ner_input_layer")
+ners_dense = Dense(int((ners_seq_length/2)), activation="relu", name="ners_dense_layer")(ners_input_layer)
+
+merged = concatenate([flated_conv_layers, events_dense, ners_dense], name="conv_event_merge_layer")
 
 dense = Dense(hidden_dims, activation="relu", name="conv_event_merge_dense_layer")(merged)
 model_output = Dense(num_labels, activation="softmax", name="Output_layer")(dense)
 
-model = Model([model_input, events_input_layer], model_output)
+model = Model([model_input, events_input_layer, ners_input_layer], model_output)
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
 #Model Summary
@@ -193,11 +168,23 @@ embedding_layer = model.get_layer("embedding_layer")
 embedding_layer.set_weights([weights])
 
 # Train the model
-model.fit([x_train, x2_train], y_train, batch_size=batch_size, epochs=num_epochs,
-          validation_data=([x_test, x2_test], y_test), verbose=2)
+model.fit([x_train, event_x_train, ners_x_train], y_train, batch_size=batch_size, epochs=num_epochs,
+          validation_data=([x_test, event_x_test, ners_x_test], y_test), verbose=2)
 
 
 model.save("model.h5")
 
-predict_movie("Hero is a lawyer or attorney in a small-town in Michigan. Bids for a reelection and loses.", model, labels_dict, event_voc, multiple = True)
+model_params = {"sequence_length": sequence_length, 
+                "events_seq_length": events_seq_length, 
+                "ners_seq_length":ners_seq_length,
+                "vocabulary": vocabulary,
+                "event_voc": event_voc,
+                "ners_voc": ners_voc,
+                "labels_dict": labels_dict}
+
+with open('model_params', 'wb') as fp:
+    pickle.dump(model_params, fp)
+    
+model, model_params = predict_helper.load_model_and_params()
+predict_helper.predict_movie("Hero is a lawyer or attorney in a small-town. Bids for a reelection and loses.", model, labels_dict, vocabulary, event_voc, ners_voc, multiple = True)
 
